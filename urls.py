19d5d60ab0dd09353817676597d3043d3e71c891
Erik Froese <erik.froese@gmail.com>
2011-08-05 00:18:05 -0400

    Remount the timecard app at /timecard/

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.base import RedirectView
 from django.views.generic.dates import MonthArchiveView, YearArchiveView
-from timecard.models import TimeEntry
+import timecard.urls
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,21 +11,7 @@ urlpatterns = patterns('',
 	# use time entry as the landing page
 	url(r'^$', RedirectView.as_view(url = 'admin/timecard/timeentry/add/')),
 
-	# report by year
-	url(r'^report/(?P<year>\d{4})/$',
-		YearArchiveView.as_view(
-			date_field = 'date',
-			queryset = TimeEntry.objects.all())),
-
-	# report by year, month
-	url(r'^report/(?P<year>\d{4})/(?P<month>\d{2})/$',
-		MonthArchiveView.as_view(
-			date_field = 'date',
-			month_format = '%m',
-			queryset = TimeEntry.objects.all())),
-
-	# url(r'^$', 'timeclock.views.home', name='home'),
-	# url(r'^timeclock/', include('timeclock.foo.urls')),
+    url(r'^timecard/', include(timecard.urls)),
 
 	# Uncomment the admin/doc line below to enable admin documentation:
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
